ce00a65749df29bbbf0efa2964335c155ad949cf
Piotr <piotr.k.matusz@gmail.com>
2018-09-09 15:09:02 +0200

    Added spring boot run configurations.

--- client/src/main/java/piotr/messenger/client/ClientMain.java
+++ client/src/main/java/piotr/messenger/client/ClientMain.java
@@ -1,12 +1,12 @@
 package piotr.messenger.client;
 
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import piotr.messenger.client.gui.MainWindow;
 
 import javax.swing.SwingUtilities;
 
+@SpringBootApplication
 public class ClientMain {
-
-//    public boolean isItTrue
 
 	public static void main(String[] args) {
 
--- server/src/main/java/piotr/messenger/server/ServerMain.java
+++ server/src/main/java/piotr/messenger/server/ServerMain.java
@@ -1,8 +1,10 @@
 package piotr.messenger.server;
 
 
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import piotr.messenger.server.core.ServerWorker;
 
+@SpringBootApplication
 public class ServerMain {
 
 	public static void main(String[] args) {
